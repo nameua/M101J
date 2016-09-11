4bc51f870f7cbef417071550d7b6a93209d24765
tslepkan <tykhon.slepkan@kredito.de>
2016-09-12 02:35:55 +0300

    Add a fruitPicker file

--- src/main/java/com/mongodb/SparkFromHandling.java
+++ src/main/java/com/mongodb/SparkFromHandling.java
@@ -5,31 +5,27 @@ import freemarker.template.Template;
 import spark.Spark;
 
 import java.io.StringWriter;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.halt;
-
 /**
- * Created by tslepkan on 9/11/16
+ * Created by tslepkan on 9/12/16
  */
-public class HelloWorldSparkFreemarkerStyle {
+public class SparkFromHandling {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
         configuration.setClassForTemplateLoading(HelloWorldSparkFreemarkerStyle.class, "/");
         StringWriter writer = new StringWriter();
-        Map<String, Object> helloMap = new HashMap<>();
         try {
-            Template helloTemplate = configuration.getTemplate("hello.ftl");
-            helloMap.put("name", "Freemarket");
-            helloTemplate.process(helloMap, writer);
+            Template fruitTemplate = configuration.getTemplate("fruitPicker.ftl");
+            Map<String, Object> fruitsMap = new HashMap<String, Object>();
+            fruitsMap.put("fruits", Arrays.asList("apple","banana","orange","peach"));
+            fruitTemplate.process(fruitsMap, writer);
         } catch (Exception e) {
-            halt(500);
             e.printStackTrace();
         }
-        Spark.get("/", (req, res) -> writer);
-
         Spark.get("/test", (req, res) -> "This is a test page");
-
+        Spark.post("/", (req, res) -> writer);
     }
 }
